ba1fb704da7f21ade1bbddc02004c64efac3820a
Sabrina Bonanno <sabrina_bonanno@hotmail.com>
2022-10-28 18:50:51 -0300

    Texto my_app viajes

--- my_app/models.py
+++ my_app/models.py
@@ -3,9 +3,10 @@ from django.db import models
 class Viaje(models.Model):
     name = models.CharField(max_length=40)
     year = models.IntegerField()
+    description = models.TextField
 
     def __str__(self):
-        return f"{self.name} - {self.year}"
+        return f"{self.name} - {self.year} - {self.description}"
 
 
 
