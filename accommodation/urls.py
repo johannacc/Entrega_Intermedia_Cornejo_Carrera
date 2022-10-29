38c6fcdab0e69504ad26f7e0edbe95660249fa0f
Johanna Cornejo <johanna.cornejo@hotmail.com>
2022-10-29 19:38:12 -0300

    actualización README

--- accommodation/urls.py
+++ accommodation/urls.py
@@ -5,6 +5,6 @@ from accommodation import views
 app_name = "accommodation"
 urlpatterns = [
     path("accommodations/", views.accommodations, name="accommodation-list"),
-    #path("accommodation/add", views.create_accommodation, name="accommodation-add"),
+    path("accommodation/add", views.create_accommodation, name="accommodation-add"),
 ]
 
